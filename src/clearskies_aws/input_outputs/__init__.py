b4473128cab06eabfe0643b2807335e12394f611
Tom Nijboer <tom.nijboer@cimpress.com>
2025-10-09 15:03:34 +0200

    feat: combine the v1 / v2 gateway to one

--- src/clearskies_aws/input_outputs/__init__.py
+++ src/clearskies_aws/input_outputs/__init__.py
@@ -3,11 +3,9 @@ from __future__ import annotations
 from clearskies_aws.input_outputs.cli_web_socket_mock import CLIWebSocketMock
 from clearskies_aws.input_outputs.lambda_alb import LambdaALB
 from clearskies_aws.input_outputs.lambda_api_gateway import LambdaAPIGateway
-from clearskies_aws.input_outputs.lambda_api_gateway_v2 import LambdaAPIGatewayV2
 from clearskies_aws.input_outputs.lambda_api_gateway_web_socket import (
     LambdaAPIGatewayWebSocket,
 )
-from clearskies_aws.input_outputs.lambda_http_gateway import LambdaHTTPGateway
 from clearskies_aws.input_outputs.lambda_invocation import LambdaInvocation
 from clearskies_aws.input_outputs.lambda_sns import LambdaSns
 from clearskies_aws.input_outputs.lambda_sqs_standard import LambdaSqsStandard
@@ -15,10 +13,8 @@ from clearskies_aws.input_outputs.lambda_sqs_standard import LambdaSqsStandard
 __all__ = [
     "CLIWebSocketMock",
     "LambdaAPIGateway",
-    "LambdaAPIGatewayV2",
     "LambdaAPIGatewayWebSocket",
     "LambdaALB",
-    "LambdaHTTPGateway",
     "LambdaInvocation",
     "LambdaSns",
     "LambdaSqsStandard",
--- src/clearskies_aws/input_outputs/lambda_http_gateway.py
+++
@@ -1,7 +0,0 @@
-from __future__ import annotations
-
-from clearskies_aws.input_outputs import lambda_api_gateway_v2
-
-
-class LambdaHTTPGateway(lambda_api_gateway_v2.LambdaAPIGatewayV2):
-    """LambdaAPIGatewayV2."""
