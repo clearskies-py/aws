9d322044e6231240d1de55adf03872adf1efdd1b
Tom Nijboer <tom.nijboer@cimpress.com>
2025-10-10 21:17:54 +0200

    tests: add parameter_store tests

--- tests/secrets/test_parameter_store.py
+++ tests/secrets/test_parameter_store.py
@@ -4,20 +4,28 @@ import unittest
 from types import SimpleNamespace
 from unittest.mock import MagicMock
 
-import pytest
+import clearskies
 
 from clearskies_aws.secrets.parameter_store import ParameterStore
 
 
 class ParameterStoreTest(unittest.TestCase):
     def setUp(self):
+        parameter_store = SimpleNamespace(get_parameter=MagicMock(return_value={"Parameter": {"Value": "sup"}}))
+        self.boto3 = SimpleNamespace(client=MagicMock(return_value=parameter_store))
+        self.botocore = SimpleNamespace(client=SimpleNamespace(ClientError=Exception))
         self.environment = SimpleNamespace(get=MagicMock(return_value="us-east-1"))
 
-    @pytest.mark.broken
     def test_get(self):
-        ssm = SimpleNamespace(get_parameter=MagicMock(return_value={"Parameter": {"Value": "sup"}}))
-        boto3 = SimpleNamespace(client=MagicMock(return_value=ssm))
-        parameter_store = ParameterStore(boto3, self.environment)
-        self.assertEqual("sup", parameter_store.get("/my/item"))
-        ssm.get_parameter.assert_called_with(Name="/my/item", WithDecryption=True)
-        boto3.client.assert_called_with("ssm", region_name="us-east-1")
+
+        def test_parameter_store(parameter_store: ParameterStore):
+            parameter_store.get("/my/item")
+            return parameter_store
+
+        context = clearskies.contexts.Context(
+            clearskies.endpoints.Callable(test_parameter_store),
+            classes=[ParameterStore],
+            bindings={"boto3": self.boto3, "environment": self.environment},
+        )
+        (status_code, response_data, response_headers) = context()
+        self.assertEqual(status_code, 200)
